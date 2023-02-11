470f6fc2736d925aa3e7a35700b631b0436d593d
jambotrn <jambotrn@gmeil.com>
2023-02-11 11:17:21 +0530

    2nd commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer(){
     return(
     <>
         <footer className="footer">
-         <p>&copy; 2022 All Rights Reserved. &nbsp; &nbsp; Designed & Developed by Sk Amirul</p>
+        <p>&copy; 2022 All Rights Reserved. &nbsp; &nbsp; Designed & Developed by Sk Amirul</p>
         </footer>
         <script defer src="https://kit.fontawesome.com/734b7ebc90.js" crossOrigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
